3d88e4557475c8cb980d58c1f14a9bc7e7b3c96b
thendricky <136391742+thendricky@users.noreply.github.com>
2023-07-16 13:18:44 -0300

    Add files via upload

---
+++ Mystery Clickers - Inf.lua
@@ -0,0 +1,27 @@
+
+
+local v1 = 999999999999999999999999999999999999999999999999999999999999999
+local v2 = "Rebirths"
+local v3 = false
+local v4 = false
+local v5 = "Normal"
+local event = game:GetService("ReplicatedStorage").Events.FreeGifts.Gift1
+event:FireServer(v1, v2, v3, v4, v5)
+
+
+local v1 = 999999999999999999999999999999999999999999999999999999999999999
+local v2 = "Clicks"
+local v3 = false
+local v4 = false
+local v5 = "Normal"
+local event = game:GetService("ReplicatedStorage").Events.FreeGifts.Gift1
+event:FireServer(v1, v2, v3, v4, v5)
+
+
+local v1 = 999999999999999999999999999999999999999999999999999999999999999
+local v2 = "Gems"
+local v3 = false
+local v4 = false
+local v5 = "Normal"
+local event = game:GetService("ReplicatedStorage").Events.FreeGifts.Gift1
+event:FireServer(v1, v2, v3, v4, v5)
